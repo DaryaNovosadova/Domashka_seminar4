c19d3a944d5b0f6a880465a984cd0ea876f2489c
Darya <darya.novosadova@gmail.com>
2022-11-09 22:08:46 +0700

    цикл возведения числа А в степень числа В

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натурально число А: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите натуральное число В: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+int fact = 1;
+int count = 0;
+int result = 0;
+
+while(count < B)
+{
+    result = A * fact;
+    fact = A * fact;
+    count++;
+}
+Console.Write(result);
