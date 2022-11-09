029b0dd0458f75242e3538bf371d3d5c343c42f4
Darya <darya.novosadova@gmail.com>
2022-11-09 21:44:39 +0700

    Сумма цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int sum = 0;
+int rem = 0;
+
+while(n > 0)
+{
+    rem = n % 10;
+    sum = sum + rem;
+    n = n/10;
+}
+Console.Write(sum);
